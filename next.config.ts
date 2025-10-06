1babe019a1a21664b53d6d44efabc8737fec0a3d
DewanshPal <dewanshpal108@gmail.com>
2025-10-07 01:04:12 +0530

    ui

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  ignoreDuringBuilds: true, // ✅ disables ESLint checks during build
 };
 
 export default nextConfig;
